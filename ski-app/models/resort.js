9e362fd6a713a42b3a57df4ff6f9acc0f4773664
sarahemilym <sarahemendham@gmail.com>
2017-01-12 15:26:44 +0000

    correct data logging

--- ski-app/models/resort.js
+++ ski-app/models/resort.js
@@ -1,8 +1,10 @@
-// const mongoose = require('mongoose');
-//
-// const resortSchema = new mongoose.Schema({
-//   id: { type: Number },
-//   name: { type: String, required: true }
-// });
-//
-// module.exports = mongoose.model('Resort', resortSchema);
+const mongoose = require('mongoose');
+
+const resortSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  region: { type: String, required: true },
+  lat: { type: String, required: true },
+  lng: { type: String, required: true}
+});
+
+module.exports = mongoose.model('Resort', resortSchema);
--- ski-app/public/js/app.js
+++ ski-app/public/js/app.js
@@ -3,9 +3,17 @@ console.log('working');
 $(init);
 
 function init(){
-  $.get('http://localhost:3000/api/resorts').done(function(data) {
-    $.each(data, function(i, resort) {
-      console.log(data);
-    });
-  });
+  const $API = $.get('http://localhost:3000/api/resorts');
+  $API.done(data => display(data));
+
+  function display (data) {
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      // console.log(data[i].SkiArea.name);
+      // console.log(data[i].Region[0].name);
+      console.log(data[i].SkiArea.geo_lat + data[i].SkiArea.geo_lng);
+      debugger
+    }
+    // console.log(data);
+  }
 }
