e9a09fda0a7d5cfd8c6a8414ca66540e362db5da
sarahemilym <sarahemendham@gmail.com>
2017-01-14 11:57:15 +0000

    map and authentication working

--- ski-app/index.js
+++ ski-app/index.js
@@ -20,14 +20,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
-// app.use('/api', expressJWT({ secret: config.secret })
-// .unless({
-//   path: [
-//     { url: '/api/login', methods: ['POST'] },
-//     { url: '/api/register', methods: ['POST'] }
-//   ]
-// }));
-// app.use(jwtErrorHandler);
+app.use('/api', expressJWT({ secret: config.secret })
+.unless({
+  path: [
+    { url: '/api/login', methods: ['POST'] },
+    { url: '/api/register', methods: ['POST'] },
+    { url: '/api/resorts', methods: ['GET'] }
+  ]
+}));
+app.use(jwtErrorHandler);
 
 function jwtErrorHandler(err, req, res, next){
   if (err.name !== 'UnauthorizedError') return next();
