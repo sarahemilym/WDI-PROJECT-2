232779d7ae68bcc16a351a08281e1b9925c66187
sarahemilym <sarahemendham@gmail.com>
2017-01-16 19:50:48 +0000

    countries added

--- ski-app/db/seeds.js
+++ ski-app/db/seeds.js
@@ -14,13 +14,16 @@ rp('https://skimap.org/SkiAreas/index.json')
     json.forEach((result, i) => {
       try {
         console.log(`${i+1}. Creating ${result.SkiArea.name}, country ${result.Region[0].RegionsSkiArea.temp_country}`);
-        Resort.create({
-          name: result.SkiArea.name,
-          region: result.Region[0].name,
-          country: result.Region[0].RegionsSkiArea.temp_country,
-          lat: result.SkiArea.geo_lat,
-          lng: result.SkiArea.geo_lng
-        });
+
+        if (result.Region[0].RegionsSkiArea.temp_country) {
+          Resort.create({
+            name: result.SkiArea.name,
+            region: result.Region[0].name,
+            country: result.Region[0].RegionsSkiArea.temp_country,
+            lat: result.SkiArea.geo_lat,
+            lng: result.SkiArea.geo_lng
+          });
+        }
       } catch(e) {
         console.log('Incorrect fields');
       }
--- ski-app/src/js/app.js
+++ ski-app/src/js/app.js
@@ -12,34 +12,77 @@ googleMap.mapSetup = function() {
   };
 
   this.map = new google.maps.Map(canvas, mapOptions);
-  // this.createDropdown();
+  this.getCountries();
   this.getResorts();
 };
 
-googleMap.createDropdown = function(resort) {
-  $('.dropdown-menu').append(`<li class="filter" value="${resort.country}"><a href="#">${resort.country}</a></li>`);
-  var $country = $('.filter').val();
-  if ($country.selected === true) {
-    console.log($country);
-  }
+googleMap.getCountries = function() {
+  $.get('http://localhost:3000/api/resorts').done(data => {
+    const codes = [];
+    const resortValue = data.map(resort => resort.country);
+    resortValue.some((code, idx) => {
+      if(resortValue.indexOf(code) === idx) {
+        codes.push(code);
+      }
+    });
+    googleMap.createDropdown(codes);
+    // console.log(codes);
+  });
+};
+
+
+    // const countryCodes = {
+    //   "38" : "England"
+    // }
+googleMap.createDropdown = function(codes) {
+  $.each(codes, function(i, val) {
+    $('.dropdown-menu').append(`<li><a href="#" class="filter" value="${val}">${val}</a></li>`);
+  });
+  $('.filter').on('click', googleMap.selectCountry);
+};
+//
+//       // const countryName = countryCodes[resort.country];
+//
+//       // console.log(resort.country);
+//       $('.dropdown-menu').append(`<li><a href="#" class="filter" value="codes[0]">codes[0]</a></li>`);
+//       $('.filter').on('click', googleMap.selectCountry);
+//       // var $country = $('.filter').val();
+//       // if ($country.selected === true) {
+//       //   console.log($country);
+//     });
+//   };
+
+googleMap.selectCountry = function(e) {
+  if (e) e.preventDefault();
+  var $country = this.innerHTML;
+  // googleMap.getResorts($country);
+  console.log('clicked', this.innerHTML);
+  // var $country = this.value;
+  googleMap.getResorts($country);
+  // console.log($country);
 
 };
 
-googleMap.getResorts = function(region) {
+googleMap.getResorts = function($country) {
   $.get('http://localhost:3000/api/resorts').done(data => {
     // console.log(data);
-    if (region){
+    if ($country){
+      console.log('country', $country);
       data = data.filter(function(resort){
-        return resort.region === region;
-      });
-    }
+        console.log('return')
+          return resort.country === $country;
+        });
+        // console.log('resort', resort.country, $country)
+        // console.log('data', $country);
+        // console.log('country2', $country)
+      };
     this.loopThroughResorts(data);
   });
 };
 
 googleMap.loopThroughResorts = function(data) {
   $.each(data, (i, resort) => {
-    googleMap.createDropdown(resort);
+    // googleMap.createDropdown(resort);
     googleMap.createMarkerForResort(resort);
   });
 };
@@ -95,7 +138,7 @@ googleMap.addInWindowForResort = function(resort, marker) {
       if (typeof googleMap.infoWindow !== 'undefined') googleMap.infoWindow.close();
       console.log(typeof googleMap.infoWindow); //does't know what this.infoWindow is gets undefined even when open
       this.infoWindow = new google.maps.InfoWindow({
-        content: `<p>${resort.name}</p><p>${resort.region}</p><p>Temperature is ${data.main.temp} ℃</p><p>Min temperature is ${data.main.temp_min} ℃</p><p>Max temperature is ${data.main.temp} ℃</p><p>Weather is ${data.weather[0].description}</p><p>Wind Speed is ${data.wind.speed}</p><img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="icon"><input type="button" id="forecast"/><a href="#" id="flights">Find flights</a>`
+        content: `<p>${resort.name}</p><p>${resort.region}</p><p>${resort.country}</p><p>Temperature is ${data.main.temp} ℃</p><p>Min temperature is ${data.main.temp_min} ℃</p><p>Max temperature is ${data.main.temp} ℃</p><p>Weather is ${data.weather[0].description}</p><p>Wind Speed is ${data.wind.speed}</p><img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="icon"><input type="button" id="forecast"/><a href="#" id="flights">Find flights</a>`
       });
       googleMap.addForecast(resort);
       googleMap.openFlightModal();
@@ -135,121 +178,121 @@ googleMap.addForecast = function(resort) {
   };
 
 
-googleMap.openFlightModal = function() {
-  $('#map-canvas').on('click', '#flights', (e) => {
-    if (e) e.preventDefault();
-    $('.modal-content').html(`
-      <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      <h4 class="modal-title">Choose Flights</h4>
-      </div>
-      <div class="modal-body">
-      <div class="form-group">
-      <label for="project_title">Title</label>
-      <input class="form-control" type="text" name="project[title]" id="project_title" placeholder="Title">
-      </div>
-      <div class="form-group">
-      <label for="flight_origin">Origin</label>
-      <input class="form-control" type="text" id="flight_origin" placeholder="Origin">
-      </div>
-      <div class="form-group">
-      <label for="flight_destination">Destination</label>
-      <input class="form-control" type="text" id="flight_destination" placeholder="Destination">
-      </div>
-      <div class="form-group">
-      <label for="fligh_date">Travel Date</label>
-      <input class="form-control" type="date" id="flight_date" placeholder="Travel Date yyyy-mm-dd">
-      </div>
-      <div class="form-group">
-      <label for="flight_passengers">Number of Passengers</label>
-      <input class="form-control" type="number" id="flight_passengers" placeholder="Number of passengers">
-      </div>
-      </div>
-      <div class="modal-footer">
-      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      <button type="submit" class="btn btn-primary" id="search">Search</button>
-      </div>
-      </form>`);
+  googleMap.openFlightModal = function() {
+    $('#map-canvas').on('click', '#flights', (e) => {
+      if (e) e.preventDefault();
+      $('.modal-content').html(`
+        <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Choose Flights</h4>
+        </div>
+        <div class="modal-body">
+        <div class="form-group">
+        <label for="project_title">Title</label>
+        <input class="form-control" type="text" name="project[title]" id="project_title" placeholder="Title">
+        </div>
+        <div class="form-group">
+        <label for="flight_origin">Origin</label>
+        <input class="form-control" type="text" id="flight_origin" placeholder="Origin">
+        </div>
+        <div class="form-group">
+        <label for="flight_destination">Destination</label>
+        <input class="form-control" type="text" id="flight_destination" placeholder="Destination">
+        </div>
+        <div class="form-group">
+        <label for="fligh_date">Travel Date</label>
+        <input class="form-control" type="date" id="flight_date" placeholder="Travel Date yyyy-mm-dd">
+        </div>
+        <div class="form-group">
+        <label for="flight_passengers">Number of Passengers</label>
+        <input class="form-control" type="number" id="flight_passengers" placeholder="Number of passengers">
+        </div>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" id="search">Search</button>
+        </div>
+        </form>`);
 
-    $('.modal').modal('show');
-  });
-  // googleMap.findFlights();
-};
+        $('.modal').modal('show');
+      });
+      // googleMap.findFlights();
+    };
 
-// googleMap.findFlights = function() {
-//   $('#map-canvas').on('click', '#search', (e) => {
-//   if (e) e.preventDefault();
-//   const $origin = $('#flight_origin').val();
-//   const $destination = $('#flight_destination').val();
-//   var FlightRequest = {
-//       'request': {
-//         'slice': [
-//           {
-//             'origin': $origin,
-//             'destination': $destination,
-//             // 'date': '2017-02-11'
-//           }
-//         ],
-//       //   'passengers': {
-//       //     'adultCount': 1,
-//       //     'infantInLapCount': 0,
-//       //     'infantInSeatCount': 0,
-//       //     'childCount': 0,
-//       //     'seniorCount': 0
-//       //   },
-//       //   'solutions': 10,
-//       //   'refundable': false
-//     }
-//   };
-//     // googleMap.searchFlights();
-// });
-// };
+    // googleMap.findFlights = function() {
+    //   $('#map-canvas').on('click', '#search', (e) => {
+    //   if (e) e.preventDefault();
+    //   const $origin = $('#flight_origin').val();
+    //   const $destination = $('#flight_destination').val();
+    //   var FlightRequest = {
+    //       'request': {
+    //         'slice': [
+    //           {
+    //             'origin': $origin,
+    //             'destination': $destination,
+    //             // 'date': '2017-02-11'
+    //           }
+    //         ],
+    //       //   'passengers': {
+    //       //     'adultCount': 1,
+    //       //     'infantInLapCount': 0,
+    //       //     'infantInSeatCount': 0,
+    //       //     'childCount': 0,
+    //       //     'seniorCount': 0
+    //       //   },
+    //       //   'solutions': 10,
+    //       //   'refundable': false
+    //     }
+    //   };
+    //     // googleMap.searchFlights();
+    // });
+    // };
 
-// googleMap.searchFlights = function() {
-//   $('main').on('click', '#search', (e) => {
-//     if (e) e.preventDefault();
-//     const formData = $(this).serialize();
-//     console.log('clicked');
-//   console.log($origin);
-// });
-// };
-//     $.ajax({
-//       type: 'POST',
-//      //Set up your request URL and API Key.
-//       url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBeNMXTnV9y9muXtJCm-5BlC5sG1YRsVA0',
-//       contentType: 'application/json', // Set Content-type: application/json
-//       dataType: 'json',
-//      // The query we want from Google QPX, This will be the variable we created in the beginning
-//       data: JSON.stringify(FlightRequest),
-//       success: function (data) {
-//       //Once we get the result you can either send it to console or use it anywhere you like.
-//         console.log(JSON.stringify(data.trips.data.airport[0].name));
-//       },
-//       error: function(){
-//        //Error Handling for our request
-//         alert('Access to Google QPX Failed.');
-//       }
-//     });
-//   });
-// };
+    // googleMap.searchFlights = function() {
+    //   $('main').on('click', '#search', (e) => {
+    //     if (e) e.preventDefault();
+    //     const formData = $(this).serialize();
+    //     console.log('clicked');
+    //   console.log($origin);
+    // });
+    // };
+    //     $.ajax({
+    //       type: 'POST',
+    //      //Set up your request URL and API Key.
+    //       url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBeNMXTnV9y9muXtJCm-5BlC5sG1YRsVA0',
+    //       contentType: 'application/json', // Set Content-type: application/json
+    //       dataType: 'json',
+    //      // The query we want from Google QPX, This will be the variable we created in the beginning
+    //       data: JSON.stringify(FlightRequest),
+    //       success: function (data) {
+    //       //Once we get the result you can either send it to console or use it anywhere you like.
+    //         console.log(JSON.stringify(data.trips.data.airport[0].name));
+    //       },
+    //       error: function(){
+    //        //Error Handling for our request
+    //         alert('Access to Google QPX Failed.');
+    //       }
+    //     });
+    //   });
+    // };
 
-//   $.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${resort.lat}&lon=${resort.lng}&units=metric&APPID=17716dc84c929276085ec7322162e7f3`).done(function(data) {
-//     console.log(data);
-//   });
-// };
-//     $('.modal-content').html(`
-//       <div class='modal-header'>
-//         <button type='button' class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-//         <h4 class="modal-title">Forecast</h4>
-//       </div>
-//       <div class="modal-body">
-//         <p>Forecast will go here</p>
-//       </div>
-//       <div class="modal-footer">
-//         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-//       </div>`);
-// $('.modal').modal('show');
-// }
+    //   $.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${resort.lat}&lon=${resort.lng}&units=metric&APPID=17716dc84c929276085ec7322162e7f3`).done(function(data) {
+    //     console.log(data);
+    //   });
+    // };
+    //     $('.modal-content').html(`
+    //       <div class='modal-header'>
+    //         <button type='button' class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    //         <h4 class="modal-title">Forecast</h4>
+    //       </div>
+    //       <div class="modal-body">
+    //         <p>Forecast will go here</p>
+    //       </div>
+    //       <div class="modal-footer">
+    //         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    //       </div>`);
+    // $('.modal').modal('show');
+    // }
 
 
-$(googleMap.mapSetup.bind(googleMap));
+    $(googleMap.mapSetup.bind(googleMap));
